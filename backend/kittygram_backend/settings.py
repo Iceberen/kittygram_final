5ed2fa350b22a9ab864dcdc600347aea0371d808
Vyacheslav Vasilyev <vgv1tpu@yandex.ru>
2025-03-19 19:41:49 +0300

    fix settings_3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -18,8 +18,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['infra-sprint1.zapto.org', 
-                 os.getenv('ALLOWED_HOSTS', 'localhost').split(',')]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
